764ddd5f4594af2a45463c013a782307e44c80af
Allal El Hommad <allalelhommad@mar-101-160.eduroamcm.upf.edu>
2024-04-03 16:28:19 +0200

    model.py changed to not be trained when imported

--- model.py
+++ model.py
@@ -90,75 +90,78 @@ class MyModel(nn.Module):
         x = self.fc3(x)  # No hay función de activación para la capa de salida
         return x
 
-# Inicializar tu modelo
-#input_size = X_tensor.shape[1]
-input_size = 34
-model = MyModel(input_size)
 
-# Definir la función de pérdida y el optimizador
-criterion = nn.BCEWithLogitsLoss()  # Pérdida de entropía cruzada binaria para la clasificación binaria
-optimizer = optim.Adam(model.parameters(), lr=0.001)  # Optimizador Adam con tasa de aprendizaje 0.001
 
-# Definir el programador de la tasa de aprendizaje
-scheduler = StepLR(optimizer, step_size=10, gamma=0.1)  # Reducir la tasa de aprendizaje a la mitad cada 10 épocas
+if __name__ == "__main__":
+    # Inicializar tu modelo
+    #input_size = X_tensor.shape[1]
+    input_size = 34
+    model = MyModel(input_size)
 
-# Bucle de entrenamiento
-num_epochs = 30
-for epoch in range(num_epochs):
-    model.train()  # Poner el modelo en modo de entrenamiento
-    train_loss = 0.0
-    for pdb_id, loader in train_loaders.items():
-        for inputs, labels in loader:
-            optimizer.zero_grad()  # Poner a cero los gradientes
-            outputs = model(inputs)  # Paso hacia adelante
-            labels = labels.view(-1, 1)
-            loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
-            loss.backward()  # Paso hacia atrás
-            optimizer.step()  # Actualizar los pesos
-            train_loss += loss.item() * inputs.size(0)
-        train_loss /= len(loader.dataset)
+    # Definir la función de pérdida y el optimizador
+    criterion = nn.BCEWithLogitsLoss()  # Pérdida de entropía cruzada binaria para la clasificación binaria
+    optimizer = optim.Adam(model.parameters(), lr=0.001)  # Optimizador Adam con tasa de aprendizaje 0.001
 
-    # Imprimir las estadísticas de entrenamiento
-    print(f'Epoch {epoch + 1}/{num_epochs}, Training Loss: {train_loss:.4f}')
+    # Definir el programador de la tasa de aprendizaje
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)  # Reducir la tasa de aprendizaje a la mitad cada 10 épocas
 
-    # Validación
-    model.eval()  # Poner el modelo en modo de evaluación
-    val_loss = 0.0
-    for pdb_id, loader in val_loaders.items():
-        for inputs, labels in loader:
-            outputs = model(inputs)  # Paso hacia adelante
-            labels = labels.view(-1, 1)
-            loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
-            val_loss += loss.item() * inputs.size(0)
-        val_loss /= len(loader.dataset)
+    # Bucle de entrenamiento
+    num_epochs = 30
+    for epoch in range(num_epochs):
+        model.train()  # Poner el modelo en modo de entrenamiento
+        train_loss = 0.0
+        for pdb_id, loader in train_loaders.items():
+            for inputs, labels in loader:
+                optimizer.zero_grad()  # Poner a cero los gradientes
+                outputs = model(inputs)  # Paso hacia adelante
+                labels = labels.view(-1, 1)
+                loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
+                loss.backward()  # Paso hacia atrás
+                optimizer.step()  # Actualizar los pesos
+                train_loss += loss.item() * inputs.size(0)
+            train_loss /= len(loader.dataset)
 
-    # Imprimir las estadísticas de validación
-    print(f'Epoch {epoch + 1}/{num_epochs}, Validation Loss: {val_loss:.4f}')
+        # Imprimir las estadísticas de entrenamiento
+        print(f'Epoch {epoch + 1}/{num_epochs}, Training Loss: {train_loss:.4f}')
 
+        # Validación
+        model.eval()  # Poner el modelo en modo de evaluación
+        val_loss = 0.0
+        for pdb_id, loader in val_loaders.items():
+            for inputs, labels in loader:
+                outputs = model(inputs)  # Paso hacia adelante
+                labels = labels.view(-1, 1)
+                loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
+                val_loss += loss.item() * inputs.size(0)
+            val_loss /= len(loader.dataset)
+
+        # Imprimir las estadísticas de validación
+        print(f'Epoch {epoch + 1}/{num_epochs}, Validation Loss: {val_loss:.4f}')
+
+        # Prueba
     # Prueba
-# Prueba
-# Prueba
-# Prueba
-test_loss = 0.0
-correct_predictions = 0
-total_predictions = 0
-for pdb_id, loader in test_loaders.items():
-    for inputs, labels in loader:
-        outputs = model(inputs)  # Paso hacia adelante
-        labels = labels.view(-1, 1)
-        loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
-        test_loss += loss.item() * inputs.size(0)
+    # Prueba
+    # Prueba
+    test_loss = 0.0
+    correct_predictions = 0
+    total_predictions = 0
+    for pdb_id, loader in test_loaders.items():
+        for inputs, labels in loader:
+            outputs = model(inputs)  # Paso hacia adelante
+            labels = labels.view(-1, 1)
+            loss = criterion(outputs.squeeze(), labels.squeeze())  # Calcular la pérdida
+            test_loss += loss.item() * inputs.size(0)
 
-        # Calcular la precisión
-        predicted = torch.round(outputs)  # Redondear las salidas del modelo para obtener las predicciones
-        total_predictions += labels.size(0)
-        correct_predictions += (predicted == labels).sum().item()
+            # Calcular la precisión
+            predicted = torch.round(outputs)  # Redondear las salidas del modelo para obtener las predicciones
+            total_predictions += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
 
-    test_loss /= len(loader.dataset)
+        test_loss /= len(loader.dataset)
 
-# Imprimir las estadísticas de prueba
-print(f'Test Loss: {test_loss:.4f}')
-print(f'Test Accuracy: {correct_predictions / total_predictions * 100:.2f}%')
+    # Imprimir las estadísticas de prueba
+    print(f'Test Loss: {test_loss:.4f}')
+    print(f'Test Accuracy: {correct_predictions / total_predictions * 100:.2f}%')
 
-# Guardar el modelo
-torch.save(model.state_dict(), 'model.pth')
+    # Guardar el modelo
+    torch.save(model.state_dict(), 'model.pth')

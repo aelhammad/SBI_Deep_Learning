1a0174409d8a9c20d8f4f24fdcc993f7e75b4a5a
Allal El Hommad <allalelhommad@MBP-de-Allal.home>
2024-04-04 19:36:30 +0200

    new user warnings and verbose mode

--- main.py
+++ main.py
@@ -1,103 +1,145 @@
 import torch
+import sys
+import time  # Import time module
 from model import MyModel, preprocess_features
 from feature_extraction import ProteinFeatures
-from Bio.PDB import PDBIO, Atom, Residue, Chain, Model, Structure
-import os
 from Bio.PDB import PDBParser
 import os
+import argparse
+from Bio.PDB.PDBExceptions import PDBConstructionException
 
+def predict_pdb(pdb_file_path, verbose=False):
+    '''
+    Predict binding pocket residues in a given PDB file using a pre-trained model.
 
-# Load the pre-trained model
-input_size = 34
-model = MyModel(input_size)
-model.load_state_dict(torch.load('models/model_100.pth'))
+    Parameters:
+        pdb_file_path (str): Path to the input PDB file.
+        verbose (bool): Flag to enable verbose mode.
 
-# Ensure that the model is in evaluation mode
-model.eval()
+    Returns:
+        None
+    '''
+    try:
+        if not pdb_file_path.lower().endswith('.pdb'):
+            raise ValueError("Error: Input file must have a .pdb extension")
 
-# Load and process your pdb data using preprocess_features function
-pdb_list = []
-pdb_file = 'example_pdb/2ay2.pdb'
-# Crear un analizador PDB
-parser = PDBParser()
+        # Check if the trust_level is within the valid range
+        if args.trust_level > 0.99:
+            raise ValueError("Error: Trust level must not exceed 0.99")
+        
+        print("Performing prediction", end="", flush=True)
+        for _ in range(3):  # Print three dots
+            print(".", end="", flush=True)
+            time.sleep(1)  # Wait for one second between dots
 
-# Parsear la estructura del PDB
-structure = parser.get_structure("structure", pdb_file)
-pdb_list = [pdb_file]
-pf = ProteinFeatures(pdb_file)
-features = pf.extract_features()
-pdb_loader = preprocess_features(features)
-print(pdb_loader)
+        # Redirect stdout and stderr to suppress output and warnings
+        if not verbose:
+            sys.stdout = open(os.devnull, 'w')
+            sys.stderr = open(os.devnull, 'w')
 
-predictions = []
-with torch.no_grad():
-    for pdb_id, loader in pdb_loader.items():
-        # Check if there's only one batch in the loader
-        if len(loader) == 1:
-            # Extract the single batch
-            inputs, _ = next(iter(loader))
-            inputs = torch.nan_to_num(inputs, nan=inputs.mean())
-        else: #change this.
-            # If there are multiple batches, concatenate them into one batch
-            inputs, _ = torch.cat([batch[0] for batch in loader]), None
-            inputs = torch.nan_to_num(inputs, nan=inputs.mean())
+        # Load the pre-trained model
+        input_size = 34
+        model = MyModel(input_size)
+        model.load_state_dict(torch.load('models/model_100.pth'))
+        model.eval()
 
-        print(inputs)
-        outputs = model(inputs)
-        print(outputs)
-        predictions.extend(outputs.tolist())
+        # Load and process the PDB data using preprocess_features function
+        parser = PDBParser()
+        structure = parser.get_structure("structure", pdb_file_path)
+        pf = ProteinFeatures(pdb_file_path)
+        features = pf.extract_features()
+        pdb_loader = preprocess_features(features)
 
-# Filter the predicted binding pocket residues
-predictions = [item for sublist in predictions for item in sublist]
-binding_pocket_residues = [index for index, prediction in enumerate(predictions) if prediction > 0.8]
-print(binding_pocket_residues)
-# Parsear la estructura del PDB
-structure = parser.get_structure("structure", pdb_file)
+        predictions = []
+        with torch.no_grad():
+            for pdb_id, loader in pdb_loader.items():
+                if len(loader) == 1:
+                    inputs, _ = next(iter(loader))
+                    inputs = torch.nan_to_num(inputs, nan=inputs.mean())
+                else:
+                    inputs, _ = torch.cat([batch[0] for batch in loader]), None
+                    inputs = torch.nan_to_num(inputs, nan=inputs.mean())
 
-# Obtener el número máximo de residuos para cada cadena
-max_residues = {}
-for model in structure:
-    for chain in model:
-        residues = [res.get_id()[1] for res in chain.get_residues() if res.get_id()[0] == " "]
-        max_residues[chain.get_id()] = max(residues)
+                outputs = model(inputs)
+                predictions.extend(outputs.tolist())
 
-max_residue_A = max_residues['A']
-# Lista para almacenar las duplas (número, cadena)
-residue_chain_tuples = []
+        # Filter the predicted binding pocket residues
+        predictions = [item for sublist in predictions for item in sublist]
+        binding_pocket_residues = [index for index, prediction in enumerate(predictions) if prediction > args.trust_level]
 
-# Iterar sobre los residuos en la lista binding_pocket_residues
-for residue in binding_pocket_residues:
-    if residue <= max_residue_A:
-        # Residuo pertenece a la cadena A
-        residue_chain_tuples.append((residue, 'A'))
-    else:
-        # Residuo pertenece a la cadena B, restar el máximo de la cadena A
-        residue_chain_tuples.append((residue - max_residue_A, 'B'))
+        # Get the maximum residue number for each chain
+        max_residues = {}
+        for model in structure:
+            for chain in model:
+                residues = [res.get_id()[1] for res in chain.get_residues() if res.get_id()[0] == " "]
+                max_residues[chain.get_id()] = max(residues)
 
-# Imprimir las duplas (número, cadena)
-print(residue_chain_tuples)
+        max_residue_A = max_residues.get('A', 0)
+        residue_chain_tuples = []
 
+        # Generate residue-chain tuples
+        for residue in binding_pocket_residues:
+            if residue <= max_residue_A:
+                residue_chain_tuples.append((residue, 'A'))
+            else:
+                residue_chain_tuples.append((residue - max_residue_A, 'B'))
 
-# Crear una lista para almacenar las líneas del pocket de unión
-predicted_binding_pocket_lines = []
+        predicted_binding_pocket_lines = []
 
-# Iterar sobre las duplas (número, cadena)
-# Abrir el archivo PDB
-with open(pdb_file, 'r') as pdb_input:
-    # Iterar sobre las líneas del archivo PDB
-    for line in pdb_input:
-        if line.startswith('ATOM'):
-        # Extraer el número de residuo y la cadena de la línea actual
-            residue_number = int(line[22:26].strip())
-            chain_id = line[21]
-            for residue, chain in residue_chain_tuples:
-                if residue == residue_number and chain == chain_id:
-                    predicted_binding_pocket_lines.append(line)
-print(predicted_binding_pocket_lines)
+        output_residues = []
+        with open(pdb_file_path, 'r') as pdb_input:
+            for line in pdb_input:
+                if line.startswith('ATOM'):
+                    residue_number = int(line[22:26].strip())
+                    chain_id = line[21]
+                    residue_name = line[17:20].strip()  # Extract the residue name
+                    for residue, chain in residue_chain_tuples:
+                        if residue == residue_number and chain == chain_id:
+                            predicted_binding_pocket_lines.append(line)
+                            output_residues.append((residue_name, residue, chain))
+        output_residues = set(output_residues)
+        output_residues = list(output_residues)
+        output_residues.sort(key=lambda x: (x[2], x[1]))
 
-pdb_file_name = os.path.basename(pdb_file)
-output_pdb_file = f'predicted_binding_pocket_{pdb_file_name}'
-with open(output_pdb_file, 'w') as output_pdb:
-    output_pdb.write(''.join(predicted_binding_pocket_lines))
+        # Restore stdout and stderr
+        if not verbose:
+            sys.stdout.close()
+            sys.stderr.close()
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
 
-print(f"Predicted binding pocket residues saved to {output_pdb_file}")
+        # Determine the output directory and save the processed PDB file
+        output_dir = os.path.dirname(pdb_file_path)
+        pdb_file_name = os.path.basename(pdb_file_path)
+        output_pdb_file = os.path.join(output_dir, f'predicted_binding_pocket_{pdb_file_name}')
+        output_txt_file = os.path.join(output_dir, f'{pdb_file_name}_predicted_pocket.txt')
+
+        with open(output_txt_file, 'w') as output_txt:
+            for residue_name, residue_number, chain in output_residues:
+                output_txt.write(f"{residue_name} {residue_number} {chain}\n")
+
+        with open(output_pdb_file, 'w') as output_pdb:
+            output_pdb.write(''.join(predicted_binding_pocket_lines))
+        
+        # Print the output_residues to standard output
+        print("\nResidue Name  Residue Number  Chain")
+        for residue_name, residue_number, chain in output_residues:
+            print(f"{residue_name:<13} {residue_number:<15} {chain}")
+        print(f"Predicted binding pocket residues saved to {output_txt_file}")
+        print(f"Predicted binding pocket PDB saved to {output_pdb_file}")
+
+    except FileNotFoundError:
+        print("Error: PDB file not found.")
+    except PDBConstructionException as e:
+        print(f"Error: Unable to parse the PDB file: {e}")
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Predict binding pocket residues in a given PDB file.')
+    parser.add_argument('--trust_level', type=float, default=0.7, help='Trust level for the prediction')
+    parser.add_argument('pdb_file_path', type=str, help='Path to the input PDB file')
+    parser.add_argument('--verbose','-v', action='store_true', help='Enable verbose mode')
+
+    args = parser.parse_args()
+    predict_pdb(args.pdb_file_path, args.verbose)

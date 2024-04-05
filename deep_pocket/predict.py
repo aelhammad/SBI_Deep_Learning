53d3ecef24afe700a84d71a51cff4ba26209b56a
Allal El Hommad <allalelhommad@mar-101-87.eduroamcm.upf.edu>
2024-04-05 17:14:46 +0200

    comments

--- deep_pocket/predict.py
+++ deep_pocket/predict.py
@@ -15,7 +15,7 @@ def predict_pdb(pdb_file_path, verbose=False):
     Parameters:
         pdb_file_path (str): Path to the input PDB file.
         verbose (bool): Flag to enable verbose mode.
-        trust_level (float): Trust level for the prediction the 
+        trust_level (float): Trust level for the prediction the binding pockets going from 0.01 to 0.99, being 0.99 the most restrictive.
     Returns:
         None
     '''
